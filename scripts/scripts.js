3eded8f2de90f2439bb361cd62223d237c77b40c
Svetlana Murdoch <66595456+Svetkachalina@users.noreply.github.com>
2022-08-23 17:28:31 +0300

    Add files via upload

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,6 +2,14 @@ const buttonMen = document.querySelector('.header__button-gender_men');
 const buttonWomen = document.querySelector('.header__button-gender_women');
 const body = document.body;
 
+const buttonChange = document.querySelector('.header__button-change');
+const buttonBg = document.querySelector('.header__button-change_image');
+
+// Сменить цвет кнопок в header при смене пола
+
+//Менять цвет иконок в footer подробности в видео
+
+
 const state = {
     gender: body.classList.contains('women') ? 'women' : 'men',
 };
@@ -21,6 +29,17 @@ const changeToWomen = () => {
         state.gender = 'women';
     }
 };
+const changeToBlue = () => {
+    buttonChange.classList.add('_blue');
+    buttonBg.classList.add('_blue');
+};
+const changeToPink = () => {
+    buttonChange.classList.remove('_blue');
+    buttonBg.classList.remove('_blue');
+};
 
 buttonMen.addEventListener('click', changeToMen);
+buttonMen.addEventListener('click', changeToBlue);
 buttonWomen.addEventListener('click', changeToWomen);
+buttonWomen.addEventListener('click', changeToPink);
+
