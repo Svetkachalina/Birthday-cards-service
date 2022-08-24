cc63fd87c52ea625366bc049ce530fdd7883bccf
Svetlana Murdoch <66595456+Svetkachalina@users.noreply.github.com>
2022-08-24 11:24:36 +0300

    Add files via upload

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,23 +2,47 @@ const buttonMen = document.querySelector('.header__button-gender_men');
 const buttonWomen = document.querySelector('.header__button-gender_women');
 const body = document.body;
 
-const buttonChange = document.querySelector('.header__button-change');
-const buttonBg = document.querySelector('.header__button-change_image');
+const buttonText = document.querySelector('.header__button-change_text');
+const buttonImage = document.querySelector('.header__button-change_image');
 
-// Сменить цвет кнопок в header при смене пола
-
-//Менять цвет иконок в footer подробности в видео
-
+const cardImage = document.querySelector('.card__image');
+const cardText = document.querySelector('.card__text');
 
 const state = {
     gender: body.classList.contains('women') ? 'women' : 'men',
 };
 
+const getRandomForArr = (arr) => {
+    const randomNumber = Math.floor(Math.random() * arr.length);
+    return arr[randomNumber];
+};
+
+const getData = () => fetch('db.json').then((response) => response.json());
+
+const changeDOM = () => {
+    if (state.photo.includes('black')) {
+        cardText.style.color = '#fff';
+    } else {
+        cardText.style.color = '';
+    }
+    cardImage.src = `img/${state.photo}`;
+        cardText.innerHTML = state.text.replaceAll('\n', '<br>');
+};
+
+const getDataToCard = () => {
+    getData().then(data => {
+        state.text = getRandomForArr(data.text[state.gender]);
+        state.photo = getRandomForArr(data.photo[state.gender]);
+        changeDOM();
+    });
+};
+    
 const changeToMen = () => {
     if (state.gender !== 'men') {
         body.classList.add('men');
         body.classList.remove('women');
         state.gender = 'men';
+        getDataToCard();
     }
 };
 
@@ -27,19 +51,37 @@ const changeToWomen = () => {
         body.classList.add('women');
         body.classList.remove('men');
         state.gender = 'women';
+        getDataToCard();
     }
 };
-const changeToBlue = () => {
-    buttonChange.classList.add('_blue');
-    buttonBg.classList.add('_blue');
+// const changeToBlue = () => {
+//     buttonText.classList.add('_blue');
+//     buttonImage.classList.add('_blue');
+// };
+// const changeToPink = () => {
+//     buttonText.classList.remove('_blue');
+//     buttonImage.classList.remove('_blue');
+// };
+
+const changeText = () => {
+    getData().then(data => {
+        state.text = getRandomForArr(data.text[state.gender]);
+        changeDOM();
+    });
 };
-const changeToPink = () => {
-    buttonChange.classList.remove('_blue');
-    buttonBg.classList.remove('_blue');
+
+const changeImage = () => {
+    getData().then(data => {
+        state.photo = getRandomForArr(data.photo[state.gender]);
+        changeDOM();
+    });
 };
 
 buttonMen.addEventListener('click', changeToMen);
-buttonMen.addEventListener('click', changeToBlue);
+// buttonMen.addEventListener('click', changeToBlue);
 buttonWomen.addEventListener('click', changeToWomen);
-buttonWomen.addEventListener('click', changeToPink);
+// buttonWomen.addEventListener('click', changeToPink);
+buttonText.addEventListener('click', changeText);
+buttonImage.addEventListener('click', changeImage);
+getDataToCard();
 
